d0cb259fbc5e8df08186d480bffebdb37b177c82
faginr <rexwfagin@gmail.com>
2022-02-14 18:50:14 -0800

    Added ability to add more ingredients

--- app.py
+++ app.py
@@ -21,19 +21,55 @@ def index():
     return render_template('index.html')
     
 
-@app.route('/upload')
+@app.route('/upload/', methods=('GET', 'POST'))
 def upload():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        type = request.form['type']
+        instructions = request.form['instructions']
+        img = request.form['img']
+
+        if not title:
+            flash('Title is required')
+        # elif not type:
+        #     flash('Meal type is required')
+        elif not instructions:
+            flash('Description is required')
+
+        else:
+            conn = get_db_conn()
+            conn.execute('INSERT INTO recipes (title, type, instructions, img) VALUES (?,?,?,?)', (title, type, instructions,img))
+            conn.commit()
+            flash('Upload Successful')
+            conn.close()
+            return render_template('show.html')
+
     return render_template('upload.html')
 
-@app.route('/search')
+@app.route('/search/')
 def search():
     return render_template('search.html')
 
-@app.route('/random')
-def random():
-    return render_template('random.html')
+@app.route('/random_recipe/', methods=('GET', 'POST'))
+def random_recipe():
+    # random.seed()
+    # num = random.randint(0,3)
+    # conn = get_db_conn()
+    # recipe = conn.execute('SELECT id FROM recipes WHERE id = ?', [num]).fetchall()
+    # conn.close()
+    if request.method == 'POST':
+        conn = get_db_conn()
+        recipes = conn.execute('SELECT * FROM recipes ORDER BY RANDOM() LIMIT 1').fetchall()
+        conn.close()
+        return render_template('random_recipe.html', recipes=recipes)
 
-@app.route('/show')
+    conn = get_db_conn()
+    recipes = conn.execute('SELECT * FROM recipes ORDER BY RANDOM() LIMIT 1').fetchall()
+    conn.close()
+    return render_template('random_recipe.html', recipes=recipes)
+
+@app.route('/show/')
 def show():
     conn = get_db_conn()
     recipes = conn.execute('SELECT * FROM recipes').fetchall()
