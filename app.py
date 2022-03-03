36b6018be6eca8bfff411a213d1675bc27bcff79
faginr <rexwfagin@gmail.com>
2022-03-03 13:56:28 -0800

    Cleaned up code, added comments, minor style updates. Added jinja 2 regex splitting for neater instructions output.

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from operator import ge
-from pickle import GET
 import sqlite3
-from flask import Flask, request, redirect, render_template, flash, url_for, session
+from flask import Flask, request, redirect, render_template, url_for, session
 from itertools import groupby
 import time
 import os
@@ -12,6 +10,7 @@ def get_db_conn():
     conn.row_factory = sqlite3.Row
     return conn
 
+# Read web scraped url from external file
 def get_url(file_path):
     with open(file_path, 'r') as url_file:
         return url_file.read()
@@ -19,6 +18,8 @@ def get_url(file_path):
 # Flask setup
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '931349940'
+
+
 
 #### ROUTES ####
 
@@ -29,28 +30,9 @@ def index():
     return render_template('index.html')
     
 
-# @app.route('/upload/', methods=('GET', 'POST'))
-# def upload():
-#     conn = get_db_conn()
+##UPLOAD ROUTES##
 
-#     if request.method == 'POST':
-#         session["title"] = request.form['title']
-#         session["type"] = request.form['type']
-#         session["instructions"] = request.form['instructions']
-#         session["img"] = request.form['img']
-
-#         session["description"] = request.form['description']
-#         session["quantity"] = request.form['quantity']
-#         session["unit"] = request.form['unit']
-
-#         print(session["title"])
-
-#         conn.close()
-#         return redirect(url_for('test'))
-
-#     conn.close()
-#     return render_template('upload.html')
-
+# Upload title
 @app.route('/upload-title/', methods=('GET','POST'))
 def upload_title():
 
@@ -64,6 +46,8 @@ def upload_title():
     
     return render_template('upload-title.html')
 
+
+# Upload ingredient
 @app.route('/upload-ingredient/', methods=('GET','POST'))
 def upload_ingredient():
 
@@ -77,15 +61,12 @@ def upload_ingredient():
         session["ingredient"].append(request.form['unit'])
         session["ingredients"].append(session["ingredient"])
 
-        # session["description"] = request.form['description']
-        # session["quantity"] = request.form['quantity']
-        # session["unit"] = request.form['unit']
-
         conn.close()
         return redirect(url_for('upload_add_ingredient'))
     
     return render_template('upload-ingredient.html', title=session["title"], type=session["type"])
 
+# Upload additional ingredients
 @app.route('/upload-add-ingredient/', methods=('GET','POST'))
 def upload_add_ingredient():
 
@@ -102,6 +83,7 @@ def upload_add_ingredient():
     
     return render_template('upload-add-ingredient.html', title=session["title"], type=session["type"], ingredients=session["ingredients"])
 
+# Upload instructions
 @app.route('/upload-instructions/', methods=('GET','POST'))
 def upload_instructions():
 
@@ -115,6 +97,7 @@ def upload_instructions():
     
     return render_template('upload-instructions.html', title=session["title"], type=session["type"], ingredients=session["ingredients"])
 
+# Get Image, write keyword/read web scraped url
 @app.route('/gen-image/', methods=('GET','POST'))
 def gen_image():
 
@@ -124,16 +107,19 @@ def gen_image():
         session["word"] = request.form['word']
         conn.close()
 
+        # Write keyword to external file
         file_path = os.getcwd() + '/word.txt'
         with open(file_path, 'w') as word_file:
             word_file.write(session["word"])
     
+        # Await url response and read in url
         file_path = os.getcwd() + '/url.txt'
         url_string = get_url(file_path)
         while url_string == '':
             time.sleep(0.5)
             url_string = get_url(file_path)
     
+        # Save url to local storage
         session["url"] = url_string
 
         with open(file_path, 'w') as url_file:
@@ -144,6 +130,7 @@ def gen_image():
 
     return render_template('gen-image.html', title=session["title"], type=session["type"], ingredients=session["ingredients"], instructions=session["instructions"])
 
+# Show image, route to database submission
 @app.route('/show-image/', methods=('GET','POST'))
 def show_image():
 
@@ -154,15 +141,17 @@ def show_image():
 
     return render_template('show-image.html', title=session["title"], type=session["type"], ingredients=session["ingredients"], instructions=session["instructions"], url=session["url"], )
 
-
+# Upload, insert new record(s) to database
 @app.route('/upload/')
 def upload():
     print(session["title"])
     conn = get_db_conn()
 
+    # Insert data into recipes table
     conn.execute('INSERT INTO recipes (title, type, instructions, img) VALUES (?,?,?,?)', (session["title"], session["type"], session["instructions"],session["url"]))
     conn.commit()
 
+    # For each ingredient, insert into ingredients table and associate with recipe
     recipe_id = conn.execute('SELECT id FROM recipes WHERE title = (?)', (session["title"], )).fetchone()['id']
     for ingredient in (session["ingredients"]):
         conn.execute('INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)', (ingredient[0],ingredient[1],ingredient[2],recipe_id))
@@ -171,33 +160,42 @@ def upload():
     conn.close()
     return redirect(url_for('show', id=recipe_id))
 
-
+# Search Route
 @app.route('/search/', methods=('GET', 'POST'))
 def search(keyword="*"):
     conn = get_db_conn()
     if request.method == 'POST':
+
+        # Get query keyword from user, or else display everything
         keyword = request.form['keyword']
         if keyword != "*":
+            # Use keyword to get recipe ids that contain keyword
             tag = conn.execute('SELECT i.recipe_id FROM ingredients i JOIN recipes r ON i.recipe_id = r.id WHERE i.description = ?;', (keyword,)).fetchall()
             items = []
+            # Grab all ingredients associated with recipes containing keyword
             for r in range(len(tag)):
                 for i in range(len(tag[0])):
                     item = conn.execute('SELECT i.id, i.description, i.quantity, i.unit, r.title, r.img, r.type, r.instructions FROM ingredients i JOIN recipes r ON i.recipe_id = r.id WHERE i.recipe_id = ? ORDER BY r.title;', (tag[r][i],)).fetchall()
                     items.append(item)
+        # Otherwise, Grab everything
         else:
             items = []
             item = conn.execute('SELECT i.id, i.description, i.quantity, i.unit, r.title, r.img, r.type, r.instructions FROM ingredients i JOIN recipes r ON i.recipe_id = r.id ORDER BY r.id;').fetchall()
             items.append(item)
+
+        # Map attributes to recipes using itertools
         recipes = {}
 
         key_func = lambda t:t['title']
 
+        # groupby -> Map attributes to recipe titles
         for item in items:
             for k,g in groupby(item, key=key_func):
                 recipes[k] = list(g)
         conn.commit()
         conn.close()
         return render_template('search.html', recipes=recipes)
+    
     if keyword != "*":
         tag = conn.execute('SELECT i.recipe_id FROM ingredients i JOIN recipes r ON i.recipe_id = r.id WHERE i.description = ?;', (keyword,)).fetchall()
         items = []
@@ -223,23 +221,28 @@ def search(keyword="*"):
     conn.close()
     return render_template('search.html', recipes=recipes)
 
+# Random Recipe Route
 @app.route('/random_recipe/', methods=('GET', 'POST'))
 def random_recipe():
     conn = get_db_conn()
 
     if request.method == 'POST':
+        # Grab a random recipe from all existant recipes
         recipes = conn.execute('SELECT * FROM recipes ORDER BY RANDOM() LIMIT 1').fetchall()
-        print(recipes[0][0])
+        # Get all related ingredients
         ingredients = conn.execute('SELECT * FROM ingredients i WHERE i.recipe_id = ?', (recipes[0][0],)).fetchall()
         conn.close()
         return render_template('random_recipe.html', recipes=recipes, ingredients=ingredients)
 
+    # Grab a random recipe from all existant recipes
     recipes = conn.execute('SELECT * FROM recipes ORDER BY RANDOM() LIMIT 1').fetchall()
+    # Get all related ingredients
     ingredients = conn.execute('SELECT * FROM ingredients i WHERE i.recipe_id = ?', (recipes[0][0],)).fetchall()
     print(recipes[0][0])
     conn.close()
     return render_template('random_recipe.html', recipes=recipes, ingredients=ingredients)
 
+# Recipes Route
 @app.route('/recipes/')
 def recipes():
     conn = get_db_conn()
@@ -249,20 +252,28 @@ def recipes():
 
     return render_template('recipes.html', recipes=recipes, ingredients=ingredients)
 
+# Show Route, show one recipe based on id
 @app.route('/<int:id>/show')
+# route takes an id from a recipe
 def show(id):
     conn = get_db_conn()
+    # Get recipe that matches primary key id
     recipe = conn.execute('SELECT title, img, type, instructions FROM recipes WHERE id = ?', (id,)).fetchone()
+    # Get related ingredients
     ingredients = conn.execute('SELECT * FROM ingredients i WHERE i.recipe_id = ?', (id,)).fetchall()
     conn.close()
     return render_template('show.html', recipe=recipe, ingredients=ingredients)
 
-
+# Delete Route
 @app.route('/<int:id>delete', methods=('POST',))
+# route takes an id from a recipe
 def delete(id):
     conn = get_db_conn()
+    # First delete ingredients linked to recipe
     conn.execute('DELETE FROM ingredients WHERE recipe_id = ?', (id,))
+    # Last delete recipe
     conn.execute('DELETE FROM recipes WHERE id = ?', (id,))
     conn.commit()
     conn.close()
+    # Redirect back to recipes route
     return redirect(url_for('recipes'))
--- seed_db.py
+++ seed_db.py
@@ -18,23 +18,25 @@ cur.execute("INSERT INTO recipes (title, img, type, instructions) VALUES (?,?,?,
     'https://images.unsplash.com/photo-1614961234488-3c1ac5c28ef1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=300',
     0,
     'Step 1: Bring hot water to a boil.\
-     Step 2: pour water over oats. Enjoy!'))
+     Step 2: pour water over oats and enjoy!'))
+
 cur.execute("INSERT INTO recipes (title, img, type, instructions) VALUES (?,?,?,?)", (
     'Grilled Cheese',
     'https://images.unsplash.com/photo-1528735602780-2552fd46c7af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=300',
     1,
     'Step 1: Heat pan to medium high.\
     Step 2: Place cheese between bread.\
-    Step 3: Place sandwich in pan for 2 minutes. \
+    Step 3: Place sandwich in pan for 2 minutes.\
     Step 4: Flip sandwich to fry other side.\
     Step 5: Remove sandwich, cut and enjoy!'))
+
 cur.execute("INSERT INTO recipes (title, img, type, instructions) VALUES (?,?,?,?)", (
     'Roasted Chicken',
     'https://images.unsplash.com/photo-1598103442097-8b74394b95c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=350&q=300',
     2,
     'Step 1: Preheat Oven to 350 degree F.\
     Step 2: Rub chicken with spices.\
-    Step 3: Put chicken on baking sheet in oven for 45 minutes\
+    Step 3: Put chicken on baking sheet in oven for 45 minutes.\
     Step 4: Remove chicken and rest 5 minutes before cutting.'))
 
 # #### Ingredinets ####
@@ -43,41 +45,49 @@ cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VA
     1,
     'half cup',
     1))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Hot Water',
     1,
     'cup',
     1))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Salt',
     1,
     'pinch',
     1))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Cheese',
     2,
     'slices',
     2))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Bread',
     2,
     'slices',
     2))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Chicken',
     1,
     'whole',
     3))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Salt',
     1,
     'pinch',
     3))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Pepper',
     1,
     'pinch',
     3))
+
 cur.execute("INSERT INTO ingredients (description, quantity, unit, recipe_id) VALUES (?,?,?,?)", (
     'Thyme',
     1,
